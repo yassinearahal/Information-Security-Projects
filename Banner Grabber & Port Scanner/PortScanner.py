11dcce0fea07f6f9ba972db7a362622064fdee83
Yassine ARAHAL <100026210+TheCyberKnight777@users.noreply.github.com>
2024-02-26 13:20:52 +0100

    Update PortScanner.py

--- Banner Grabber & Port Scanner/PortScanner.py
+++ Banner Grabber & Port Scanner/PortScanner.py
@@ -4,14 +4,14 @@ import sys
 import socket
 from datetime import datetime
 
-#Define our target
+#Defining our target
 if len(sys.argv) == 2:
 	target = socket.gethostbyname(sys.argv[1]) #Translate hostname to IPv4
 else:
 	print("Invalid amount of arguments.")
 	print("Syntax: python3 scanner.py")
 
-#Add a pretty banner
+#Adding a pretty banner
 print("-" * 50)
 print("Scanning target "+target)
 print("Time started: "+str(datetime.now()))
